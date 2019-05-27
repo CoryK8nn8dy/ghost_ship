d4c4b8ac523f3f9f37a5687e22229050a1e77eaa
Cory Kennedy <corykennedy@pop-os.localdomain>
2019-05-27 13:29:22 -0600

    created 'docker -v' functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env; // for collecting command line arguments
+use std::str; // for string handling
+use std::process::Command; // for executing bash commands
+
 fn main() {
-    println!("Hello, world!");
+    // get all of the command line arguments
+    let mut args: Vec<String> = env::args().collect();
+    // replace this program's binary execution command with "docker" 
+    args[0] = "docker".to_string();
+    
+    // make system call based on arguments
+    let docker_cmd = Command::new(&args[0])
+            .arg(&args[1])
+            .output()
+            .expect("The process failed to execute");
+
+    // collect stdout and convert to string
+    let docker_out = docker_cmd.stdout;
+    let out_str = match str::from_utf8(&docker_out){
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    // print the parsed docker command that was sent to the system
+    println!("{:?}", args);
+    // print the results of the docker command
+    println!("{}", out_str);
 }
